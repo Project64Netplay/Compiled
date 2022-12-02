ea0cc6d1a416775c6a8ba71424b234196f6e24b2
Nayla <naylahanegan@gmail.com>
2022-12-02 14:33:32 -0800

    Add files via upload

--- Updater.bat
+++ Updater.bat
@@ -1,5 +1,5 @@
 @echo off
-title Project 64 Netplay Updater
+title Project64 Netplay Updater
 :choice
 set /P c=Are you sure you want to continue[Y/N]?
 if /I "%c%" EQU "Y" goto :Start
@@ -7,7 +7,7 @@ if /I "%c%" EQU "N" goto :Exit
 goto :Exit
 
 :Start
-TASKKILL /IM "Project64 Netplay.exe" /F
+TASKKILL /IM "Project64-Netplay.exe" /F
 "Plugin/Updater/wget.exe" https://github.com/Project64Netplay/win32-Binaries/archive/refs/heads/main.zip
 "Plugin/Updater/7z.exe" x main.zip
 cd Config
@@ -24,11 +24,11 @@ cd ..\..\
 rmdir Config /s /q
 rmdir Plugin /s /q
 rmdir Save /s /q
-cd win32-Binaries-main
+cd project64-mpn-main
 move Config ..\
 move Plugin ..\
 move Save ..\
-move "Project64 Netplay.exe" ..\
+move "Project64-Netplay.exe" ..\
 cd ..\
 rmdir win32-Binaries-main /s /q
 DEL main.zip
@@ -36,5 +36,6 @@ move GlideN64.ini Plugin\GFX
 move NRage.ini Config
 move Project64.cfg Config
 move netplay_input_plugin.ini Plugin\Input
+start Project64-Netplay
 :Exit
 EXIT
